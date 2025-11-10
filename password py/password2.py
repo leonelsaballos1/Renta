f94104092b9576cdeaa809419e1fac1d874b4fe4
leonelsaballos1 <leonelsaballos459@gmail.com>
2025-11-10 15:51:48 -0600

    Delete password2.py

--- password py/password2.py
+++
@@ -1,9 +0,0 @@
-
-from werkzeug.security import generate_password_hash, check_password_hash
-
-texto = "x?1_p-H"
-
-texto_encriptado = generate_password_hash(texto)
-print("Texto Encriptado:{texto_encriptado}", )
-
-print(f"¿la contraceña es correcta? {check_password_hash(texto_encriptado, texto)}")
